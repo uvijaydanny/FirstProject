63d56b831751517c7970eea4cd1a41e3eb734490
Danny Undrajana <danny.undrajana@senecaglobal.net>
2018-09-27 16:37:27 +0530

    main java class changed

--- src/main/java/com/spring/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/spring/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+//this is Eurekha naming server
 @SpringBootApplication
 @EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
