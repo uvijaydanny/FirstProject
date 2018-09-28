0c747814aeb3a5eb09fc24aab6b9a59330716378
Danny Undrajana <danny.undrajana@senecaglobal.net>
2018-09-28 09:24:37 +0530

    back to first commit

--- src/main/java/com/spring/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/spring/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-//this is Eurekha naming server
 @SpringBootApplication
 @EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
